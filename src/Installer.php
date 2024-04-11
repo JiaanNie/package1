e92f597bfbf15fce5ffe84ecb4e36b3f048086f0
wilson <wnie@peerglobe.ca>
2024-04-11 16:41:02 -0500

    testing post install cmd

--- src/Installer.php
+++ src/Installer.php
@@ -2,7 +2,6 @@
 namespace VendorDemoName\Package1\Src\Installer;
 class Installer {
     public static function install() {
-
         $projectRoot = getcwd();
         echo "installing... at $projectRoot\n";
     }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor-demo-name/package1',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '65e93a68febf0b44f597ffb944cb520d3b26bef6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor-demo-name/package1' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '65e93a68febf0b44f597ffb944cb520d3b26bef6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
